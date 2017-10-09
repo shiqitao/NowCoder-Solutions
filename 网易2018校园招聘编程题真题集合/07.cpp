b4464c887349b124c50924f9e2a5aaaca425febb
shiqitao <shiqitao@163.com>
2017-10-09 22:50:21 +0800

    2017-10-09 22:50

--- 网易2018校园招聘编程题真题集合/07.cpp
+++ 网易2018校园招聘编程题真题集合/07.cpp
@@ -9,10 +9,12 @@ int main()
 	int *data = new int[n];
 	cin >> data[0] >> data[1];
 	int *DP = new int[n*n];
-	int sum = 0;
+	int sum = 0, temp = abs(data[1] - data[0]);
 	DP[0] = DP[1] = DP[n] = 0;
 	for (int i = 2; i < n; i++) {
 		cin >> data[i];
+		DP[i*n + i - 1] = sum + temp;
+		DP[(i - 1)*n + i] = sum + temp;
 		sum += abs(data[i] - data[i - 1]);
 		DP[i] = sum;
 		DP[i*n] = sum;
@@ -22,13 +24,11 @@ int main()
 			if (i - 1 > j) DP[i*n + j] = DP[(i - 1)*n + j] + abs(data[i] - data[i - 1]);
 			else if (j - 1 > i) DP[i*n + j] = DP[i*n + j - 1] + abs(data[i] - data[i - 1]);
 			else if (i - 1 == j) {
-				DP[i*n + j] = INT_MAX;
 				for (int k = 0; k < j; k++) {
 					DP[i*n + j] = min(DP[i*n + j], DP[k*n + j] + abs(data[i] - data[k]));
 				}
 			}
 			else if (j - 1 == i) {
-				DP[i*n + j] = INT_MAX;
 				for (int k = 0; k < i; k++) {
 					DP[i*n + j] = min(DP[i*n + j], DP[i*n + k] + abs(data[j] - data[k]));
 				}
