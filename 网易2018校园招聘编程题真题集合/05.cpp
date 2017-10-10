d9ab84dc2644f805cf9abbac667f0757f427c5a4
shiqitao <shiqitao@163.com>
2017-10-10 10:44:12 +0800

    2017-10-10 10:44

--- 网易2018校园招聘编程题真题集合/05.cpp
+++ 网易2018校园招聘编程题真题集合/05.cpp
@@ -5,13 +5,18 @@ int main()
 	int t; cin >> t;
 	for (int i = 0; i < t; i++) {
 		int n; cin >> n;
-		int sum = 0,num;
+		int sum = 0, num;
+		bool iftwo = false;
 		for (int j = 0; j < n; j++) {
 			cin >> num;
-			if (num % 4 == 0) sum += 2;
+			if (num % 4 == 0) {
+				sum += 2;
+				iftwo = true;
+			}
 			else if (num % 2 == 0) sum += 1;
 		}
-		if (sum * 2 >= 2 * n - 2) cout << "Yes" << endl;
+		if (sum > n - 1) cout << "Yes" << endl;
+		else if (sum == n && iftwo) cout << "Yes" << endl;
 		else cout << "No" << endl;
 	}
 	return 0;
