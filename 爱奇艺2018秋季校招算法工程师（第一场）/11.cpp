8a8073949e2422b5ea708e85e2aaf1ea71f712fc
shiqitao <shiqitao@163.com>
2017-12-20 17:17:53 +0800

    2017-12-20 17:17

---
+++ 爱奇艺2018秋季校招算法工程师（第一场）/11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#define mod 1000000007
+using namespace std;
+int main()
+{
+	int n; cin >> n;
+	int *num = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	sort(num, num + n);
+	long long int result = 1;
+	for (int i = 0; i < n; i++) {
+		if (num[i] <= i) {
+			cout << 0 << endl;
+			return 0;
+		}
+		result = result*(num[i] - i) % mod;
+	}
+	cout << result << endl;
+	return 0;
+}
