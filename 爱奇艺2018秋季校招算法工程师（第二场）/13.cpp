411d4da2a7a552f84f558fd598eea6f71df9b5a1
shiqitao <shiqitao@163.com>
2017-12-21 15:33:24 +0800

    2017-12-21 15:33

--- 爱奇艺2018秋季校招算法工程师（第二场）/13.cpp
+++ 爱奇艺2018秋季校招算法工程师（第二场）/13.cpp
@@ -8,13 +8,7 @@ int main()
 	for (int i = 0; i < n; i++) {
 		cin >> data[i];
 	}
-	int i = 0, j = 1;
-	while (data[j] == data[i]) j++;
-	int max1 = max(data[i], data[j]);
-	int max2 = min(data[i], data[j]);
-	int index1 = data[i] > data[j] ? i : j;
-	int index2 = data[i] > data[j] ? j : i;
-	int maxor = max1^max2;
+	int i = -1, j = n, max1, max2, index1, index2, maxor;
 	while (true) {
 		if (j == n) {
 			i++; j = i + 1;
@@ -38,16 +32,7 @@ int main()
 			index2 = j; max2 = data[j];
 			maxor = max(max1^max2, maxor);
 		}
-		else {
-			i++; j = i + 1;
-			while (j != n && data[j] == data[i]) j++;
-			if (j == n) break;
-			max1 = max(data[i], data[j]);
-			max2 = min(data[i], data[j]);
-			index1 = data[i] > data[j] ? i : j;
-			index2 = data[i] > data[j] ? j : i;
-			maxor = max(max1^max2, maxor);
-		}
+		else j = n;
 	}
 	cout << maxor << endl;
 	delete[] data;
