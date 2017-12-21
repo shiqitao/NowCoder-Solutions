a99056387cd1d72a1a59442ee9158d7d57345fe3
shiqitao <shiqitao@163.com>
2017-12-21 09:27:20 +0800

    2017-12-21 09:27

--- 爱奇艺2018秋季校招算法工程师（第一场）/13.cpp
+++ 爱奇艺2018秋季校招算法工程师（第一场）/13.cpp
@@ -8,43 +8,18 @@ int main()
 	string sequence; cin >> sequence;
 	int len = sequence.length();
 	int maxlen = 0;
-	for (int i = 0; i < len; i++) {
-		for (int j = i + 1; j < len; j++) {
-			if (sequence[i] == sequence[j]) {
-				int nowlen = 1;
-				int x = j - i - 1;
-				int y = len - j - 1;
-				if (x != 0 && y != 0) {
-					int start1 = i + 1, start2 = j + 1;
-					int **DP = new int*[x];
-					for (int k = 0; k < x; k++) {
-						DP[k] = new int[y];
-						memset(DP[k], 0, sizeof(int)*y);
-					}
-					for (int k = 0; k < y; k++) {
-						if (sequence[start1] == sequence[start2 + k]) {
-							DP[0][k] = 1;
-						}
-					}
-					for (int k = 0; k < x; k++) {
-						if (sequence[start1 + k] == sequence[start2]) {
-							DP[k][0] = 1;
-						}
-					}
-					for (int p = 1; p < y; p++) {
-						for (int q = 1; q < x; q++) {
-
-						}
-					}
-
-					for (int k = 0; k < x; k++) {
-						delete[] DP[k];
-					}
-					delete[] DP;
-				}
-			}
+	for (int k = 1; k < len; k++) {
+		int **DP = new int*[k];
+		for (int i = 0; i < k; i++) {
+			DP[i] = new int[len - k];
+			memset(DP[i], 0, sizeof(int)*(len - k));
 		}
-	}
+		
 
+		for (int i = 0; i < k; i++) {
+			delete[] DP[i];
+		}
+		delete[] DP;
+	}
 	return 0;
 }
